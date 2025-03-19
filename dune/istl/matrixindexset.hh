44eeb96ef459ece4f93a32f3be5b044cc525d5cd
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2025-03-19 18:40:15 +0100

    Reuse RowIndexSet in BCRSMatrix::CreateIterator
    The BCRSMatrix with row_wise creation creates many allocations and uses very sparse memory. This leads to a relatively slow pattern creation. So this change exposes the internals of the (faster) row index set from the MatrixIndexSet class into an re-usable pattern creator for the row-wise build mode of the matrix.

--- dune/istl/matrixindexset.hh
+++ dune/istl/matrixindexset.hh
@@ -35,45 +35,141 @@ namespace Dune {
    */
   class MatrixIndexSet
   {
-    using Index = std::uint_least32_t;
-
-    // A vector that partly mimics a std::set by staying
-    // sorted on insert() and having unique values.
-    class FlatSet : public std::vector<Index>
-    {
-      using Base = std::vector<Index>;
-    public:
-      using Base::Base;
-      using Base::begin;
-      using Base::end;
-      void insert(const Index& value) {
-        auto it = std::lower_bound(begin(), end(), value);
-        if ((it == end() or (*it != value)))
-          Base::insert(it, value);
-      }
-      bool contains(const Index& value) const {
-        return std::binary_search(begin(), end(), value);
-      }
-    };
-
-    using RowIndexSet = std::variant<FlatSet, std::set<Index>>;
-
   public:
 
-    using size_type = Index;
-
     /**
-     * \brief Default value for maxVectorSize
+     * @class RowIndexSet
+     * @brief Manages a set of row indices with efficient insertion and lookup operations.
      *
-     * This was selected after benchmarking for the worst case
-     * of reverse insertion of column indices. In many applications
-     * this works well. There's no need to use a different value
-     * unless you have many dense rows with more than defaultMaxVectorSize
-     * nonzero entries. Even in this case defaultMaxVectorSize may work
-     * well and a finding a better value may require careful
-     * benchmarking.
+     * This class provides a flexible and efficient way to store and manage
+     * row indices, supporting both vector and set storage types. It automatically
+     * switches between these storage types based on the number of indices to
+     * optimize performance for different use cases.
+     *
+     * @details
+     * The `RowIndexSet` class uses a `std::variant` to store row indices either as
+     * a `std::vector` or a `std::set`. The vector storage is used initially for
+     * its cache-friendly properties and efficient iteration, while the set storage
+     * is used when the number of indices exceeds a specified threshold
+     * (`maxVectorSize`), providing efficient insertion and lookup operations.
+     *
+     * This class is not thread safe in the sense that inserting several column
+     * indices leads to a data race.
      */
-    static constexpr size_type defaultMaxVectorSize = 2048;
+    class RowIndexSet {
+    public:
+      using Index = std::uint_least32_t;
+      using size_type = Index;
+
+      /**
+       * \brief Default value for maxVectorSize
+       *
+       * This was selected after benchmarking for the worst case
+       * of reverse insertion of column indices. In many applications
+       * this works well. There's no need to use a different value
+       * unless you have many dense rows with more than defaultMaxVectorSize
+       * nonzero entries. Even in this case defaultMaxVectorSize may work
+       * well and a finding a better value may require careful
+       * benchmarking.
+       */
+      static constexpr size_type defaultMaxVectorSize = 2048;
+
+      /**
+       * @brief Constructs a RowIndexSet with a specified maximum vector size.
+       *
+       * @param maxVectorSize The maximum number of indices to store in a
+       * vector before switching to set storage.
+       */
+      RowIndexSet(size_type maxVectorSize = defaultMaxVectorSize) : maxVectorSize_{maxVectorSize}
+      {}
+
+      /**
+       * @brief Inserts a column index into the row index set.
+       *
+       * This function inserts a column index into the row index set, automatically
+       * switching to set storage if the maximum vector size is exceeded.
+       *
+       * @param col The column index to insert.
+       */
+      void insert(size_type col){
+        std::visit(Dune::overload(
+          // If row is stored as set, call insert directly
+          [&](std::set<size_type>& set) {
+            set.insert(col);
+          },
+          // If row is stored as vector only insert directly
+          // if maxVectorSize_ is not reached. Otherwise switch
+          // to set storage first.
+          [&](std::vector<Index>& sortedVector) {
+            auto it = std::lower_bound(sortedVector.cbegin(), sortedVector.cend(), col);
+            if (it == sortedVector.cend() or (*it != col)) {
+              if (sortedVector.size() < maxVectorSize_) {
+                sortedVector.insert(it, col);
+              } else {
+                std::set<size_type> set(sortedVector.cbegin(), sortedVector.cend());
+                set.insert(col);
+                storage_ = std::move(set);
+              }
+            }
+          }
+        ), storage_);
+      }
+
+      /**
+       * @brief Checks if the row index set contains a specific column index.
+       *
+       * @param col The column index to check.
+       * @return True if the column index is present in the row index set, false otherwise.
+       */
+      bool contains(const Index& col) const {
+        return std::visit(Dune::overload(
+          [&](const std::set<Index>& set) {
+            return set.contains(col);
+          },
+          [&](const std::vector<Index>& sortedVector) {
+            return std::binary_search(sortedVector.cbegin(), sortedVector.cend(), col);
+          }
+        ), storage_);
+      }
+
+      /**
+       * @brief Returns the current storage of row indices.
+       *
+       * @return A constant reference to the storage variant containing the row indices.
+       */
+      const std::variant<std::vector<Index>, std::set<Index>>& storage() const {
+        return storage_;
+      }
+
+      /**
+       * @brief Returns the number of row indices in the set.
+       *
+       * @return The number of row indices.
+       */
+      size_type size() const {
+        return std::visit([&](const auto& rowIndices) {
+          return rowIndices.size();
+        }, storage_);
+      }
+
+      /**
+       * @brief Clears all row indices from the set.
+       */
+      void clear() {
+        std::visit([&](auto& rowIndices) {
+          rowIndices.clear();
+        }, storage_);
+      }
+
+    private:
+      std::variant<std::vector<Index>, std::set<Index>> storage_;
+      size_type maxVectorSize_;
+    };
+
+
+    using size_type = typename RowIndexSet::Index;
+
+    static constexpr size_type defaultMaxVectorSize = RowIndexSet::defaultMaxVectorSize;
 
     /**
      * \brief Constructor with custom maxVectorSize
@@ -92,14 +188,14 @@ namespace Dune {
      */
     MatrixIndexSet(size_type rows, size_type cols, size_type maxVectorSize=defaultMaxVectorSize) : rows_(rows), cols_(cols), maxVectorSize_(maxVectorSize)
     {
-      indices_.resize(rows_, FlatSet());
+      indices_.resize(rows_, RowIndexSet{maxVectorSize_});
     }
 
     /** \brief Reset the size of an index set */
     void resize(size_type rows, size_type cols) {
       rows_ = rows;
       cols_ = cols;
-      indices_.resize(rows_, FlatSet());
+      indices_.resize(rows_, RowIndexSet{maxVectorSize_});
     }
 
     /**
@@ -110,25 +206,7 @@ namespace Dune {
      * columns.
      */
     void add(size_type row, size_type col) {
-      return std::visit(Dune::overload(
-        // If row is stored as set, call insert directly
-        [&](std::set<size_type>& set) {
-          set.insert(col);
-        },
-        // If row is stored as vector only insert directly
-        // if maxVectorSize_ is not reached. Otherwise switch
-        // to set storage first.
-        [&](FlatSet& sortedVector) {
-          if (sortedVector.size() < maxVectorSize_)
-            sortedVector.insert(col);
-          else if (not sortedVector.contains(col))
-          {
-            std::set<size_type> set(sortedVector.begin(), sortedVector.end());
-            set.insert(col);
-            indices_[row] = std::move(set);
-          }
-        }
-      ), indices_[row]);
+      indices_[row].insert(col);
     }
 
     /** \brief Return the number of entries */
@@ -154,15 +232,17 @@ namespace Dune {
      * of this range, the result is stored in a std::variant<...>
      * which has to be accessed using `std::visit`.
      */
-    const auto& columnIndices(size_type row) const {
+    [[deprecated("Use rowIndexSetInstead")]] const auto& columnIndices(size_type row) const {
+      return indices_[row].storage();
+    }
+
+    const RowIndexSet& rowIndexSet(size_type row) const {
       return indices_[row];
     }
 
     /** \brief Return the number of entries in a given row */
     size_type rowsize(size_type row) const {
-      return std::visit([&](const auto& rowIndices) {
-        return rowIndices.size();
-      }, indices_[row]);
+      return indices_[row].size();
     }
 
     /** \brief Import all nonzero entries of a sparse matrix into the index set
@@ -185,7 +265,7 @@ namespace Dune {
         ColumnIterator cEndIt = row.end();
 
         for(; cIt!=cEndIt; ++cIt)
-          add(rowIdx+rowOffset, cIt.index()+colOffset);
+          indices_[rowIdx+rowOffset].insert(cIt.index()+colOffset);
 
       }
 
@@ -207,11 +287,10 @@ namespace Dune {
 
       matrix.endrowsizes();
 
-      for (size_type row=0; row<rows_; row++) {
-        std::visit([&](const auto& rowIndices) {
+      for (size_type row=0; row<rows_; row++)
+        std::visit([&](const auto& rowIndices){
           matrix.setIndicesNoSort(row, rowIndices.begin(), rowIndices.end());
-        }, indices_[row]);
-      }
+        }, indices_[row].storage());
 
       matrix.endindices();
 
